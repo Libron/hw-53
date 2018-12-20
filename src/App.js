661af14d8c8f60dd2844d221319a74f125ec6d3e
Urmat Bekboev <bek.urmat@gmail.com>
2018-12-21 05:06:52 +0600

    Rendering Task component. Added remove method

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
+import Task from './components/Task/Task';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
+  state = {
+      taskList: [
+          {title: 'Сделать это', id: 'task0'},
+          {title: 'Молоко', id: 'task1'},
+          {title: 'Dmitrii', id: 'task2'},
+          {title: 'Стиать', id: 'task3'}
+      ]
+  };
 
+  removeTask = index => {
+    let id = this.state.taskList.findIndex(t => t.id === index);
+    if (id !== -1) {
+        const taskList = [...this.state.taskList];
+        taskList.splice(id, 1);
+
+        this.setState({taskList});
+    }
+  };
+
+  render() {
+    const taskList = this.state.taskList.map((task) => (
+        <Task
+            text={task.title}
+            key={task.id}
+            remove={() => this.removeTask(task.id)}
+        />
+    ));
+
+    return (
+      <div className="App container">
+          {taskList}
       </div>
     );
   }

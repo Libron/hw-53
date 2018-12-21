9b7b85587c529b50ace0e86f6bffc534e2476cb2
Urmat Bekboev <bek.urmat@gmail.com>
2018-12-21 07:05:33 +0600

    Added extra feature "Task status - done"

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -3,10 +3,10 @@ import './Task.css';
 
 const Task = props => {
     return (
-        <div className="task">
+        <div className={"task " + props.status}>
             <h4 className="item">{props.text}</h4>
             <div className="buttons">
-                <input type="checkbox" name="done" value="done" />
+                <input type="checkbox" name="done" value="done" onChange={props.checkbox} />
                 <button className="btn-remove" onClick={props.remove}> </button>
             </div>
         </div>
